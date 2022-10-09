8d10462c3759a8614246215da00ccc037080d679
Shavz <104979137+Shavz@users.noreply.github.com>
2022-10-09 20:35:38 +0200

    2. Selection sort

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,30 +1,32 @@
 #include "sort.h"
+
 /**
- *selection_sort - sorting by selection algorithm
- *@array:the array
- *@size: th array of the size
+ *selection_sort - sort using selection algorithm
+ *@array: array
+ *@size: array size
  */
+
 void selection_sort(int *array, size_t size)
 {
 	size_t pos = 0, j = 0, x;
-	int minur;
+	int minor;
 
 	if (size < 2)
 		return;
 	for (pos = 0; pos < size - 1; pos++)
 	{
-		minur = array[pos];
+		minor = array[pos];
 		x = pos;
 		for (j = pos + 1; j < size; j++)
 		{
-			if (minur > array[j])
+			if (minor > array[j])
 			{
-				minur = array[j];
+				minor = array[j];
 				x = j;
 			}
 		}
 		array[x] = array[pos];
-		array[pos] = minur;
+		array[pos] = minor;
 		if (pos != x)
 		{
 			print_array(array, size);
