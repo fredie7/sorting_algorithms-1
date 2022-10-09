6ec7250878f03400c48599e646fef4f264d0f6b2
Shavz <104979137+Shavz@users.noreply.github.com>
2022-10-09 21:11:06 +0200

    10. Bitonic sort

--- 106-bitonic_sort.c
+++ 106-bitonic_sort.c
@@ -1,12 +1,13 @@
 #include "sort.h"
 
 /**
- * bitonic_merge - sorts and merges a sequence in ascending or descending order
- * @array: array to sort
- * @l: index of the left-most element
- * @r: index of the right-most element
+ * bitonic_merge - sorts and merges a sequence
+ * @array: array
+ * @l: index of left element
+ * @r: index of right element
  * @direction: ascending or descending
  */
+
 void bitonic_merge(int *array, int l, int r, int direction)
 {
 	int tmp, i, step = (l + r) / 2, mid = (r - l + 1) / 2;
@@ -29,13 +30,14 @@ void bitonic_merge(int *array, int l, int r, int direction)
 }
 
 /**
- * bitonic_recursion - recursive function for bitonic sort
- * @array: array to sort
- * @l: index of the left-most element
- * @r: index of the right-most element
+ * bitonic_recursion - function for bitonic sort
+ * @array: array
+ * @l: index of left element
+ * @r: index of right element
  * @direction: ascending or descending
- * @size: size of the array
+ * @size: array size
  */
+
 void bitonic_recursion(int *array, int l, int r, int direction, size_t size)
 {
 	int step;
@@ -62,10 +64,11 @@ void bitonic_recursion(int *array, int l, int r, int direction, size_t size)
 }
 
 /**
- * bitonic_sort - sorts an array following the Bitonic sort algorithm
- * @array: array of ints to sort
- * @size: size of the array
+ * bitonic_sort - sort array using Bitonic sort algorithm
+ * @array: int array
+ * @size: array size
  */
+
 void bitonic_sort(int *array, size_t size)
 {
 	if (!array || size < 2)
